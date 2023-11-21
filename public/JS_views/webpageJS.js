bfbfeb1366f48cd6c3de2fe4cb7556665587a030
skrohne <krohne12@gmail.com>
2023-11-21 17:45:10 +0100

    Fertigstellung Frontend Options

---
+++ public/JS_views/webpageJS.js
@@ -0,0 +1,32 @@
+"use strict"
+
+console.log("webpageJS")
+ //Add Leaflet Map 
+ var map = L.map('map').setView([51.305915044598834,10.21774343122064], 6);
+ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+ maxZoom: 19,
+ attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+ }).addTo(map);
+
+ // Add Leaflet Draw controls
+ var drawnItems = new L.FeatureGroup();
+ map.addLayer(drawnItems);
+
+ var drawControl = new L.Control.Draw({
+     draw: {
+         rectangle: true,
+         polyline: false,
+         circle: false,
+         marker: false,
+         polygon: false,
+         circlemarker: false
+     },
+     edit: {
+     featureGroup: drawnItems
+     }
+ });
+ // Event listener for the button click
+ document.getElementById('drawButton').addEventListener('click', function() {
+     // Add Leaflet Draw controls to the map
+     map.addControl(drawControl);
+ });
