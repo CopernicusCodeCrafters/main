b66b37342e392138d0019a6f24ffefeb260a3552
kwundram2602 <134778951+kwundram2602@users.noreply.github.com>
2023-12-06 16:13:07 +0100

    Docker compose  now faster ,using built image

--- routes/webpage.js
+++ routes/webpage.js
@@ -3,8 +3,8 @@ var router = express.Router();
 var mongodb = require("mongodb");
 const { MongoClient } = require("mongodb");
 
-const url = "mongodb://127.0.0.1:27017"; 
-//const url = "mongodb://mongo:27017"; // connection URL
+//const url = "mongodb://127.0.0.1:27017"; 
+const url = "mongodb://mongo:27017"; // connection URL
 const client = new MongoClient(url); // mongodb client
 const dbName = "mydatabase"; // database name
 const collectionName = "newpois"; // collection name
