474dbd53f80dc27085cbb8d0cafc6f12a9a4a7e8
Emil <emil.erlenkoetter@web.de>
2024-01-09 16:50:43 +0100

    working url change

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var logger = require("morgan");
 var mongodb = require("mongodb");
 var engines = require('consolidate')
 
-
 const url = "mongodb://127.0.0.1:27017"
 //const url = "mongodb://mongo:27017";
 let dbName = "geosoft2";
