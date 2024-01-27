4f512fd5f1b7acd480bdc0d70a286955e5fc7af1
Emil <emil.erlenkoetter@web.de>
2024-01-27 23:37:20 +0100

    change z-index from satellite image

--- public/JS_views/webpageJS.js
+++ public/JS_views/webpageJS.js
@@ -366,7 +366,7 @@ function processGeoJSON(geojsonData) {
   //Using Turf.js to create bounding box for OpenEO Request
   var bbox = turf.bbox(geojsonData);
   var bboxPolygon = turf.bboxPolygon(bbox);
-  L.geoJSON(geojsonData).addTo(map);
+    L.geoJSON(geojsonData).addTo(map);
   L.geoJSON(bboxPolygon).addTo(map);
 
   // Define the source and destination coordinate systems
@@ -504,6 +504,7 @@ async function createDatacube() {
         let layer = new GeoRasterLayer({
           georaster: georaster,
           opacity: 1,
+          zIndex: 10,
 
           pixelValuesToColorFn: function (pixelValues) {
 
